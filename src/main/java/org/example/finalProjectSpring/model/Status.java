ca99a5df6ce351efc9ec4a32119c8d7e5b5669c3
Katya-art <katakravchenko01@gmail.com>
2021-09-27 16:07:02 +0300

    Fixed some bags

--- src/main/java/org/example/finalProjectSpring/model/Status.java
+++ src/main/java/org/example/finalProjectSpring/model/Status.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class Status {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
